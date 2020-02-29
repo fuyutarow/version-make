cc2d18824333ee8589bfcfaf54cef110851d7822
fuyutarow <fuyutarow@gmail.com>
2020-02-29 18:06:38 +0900

    Add feature: upver Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
+use toml::Value as Toml;
 
 #[derive(StructOpt, Debug)]
 enum Opt {
@@ -49,6 +50,34 @@ impl Semver for Version {
     }
 }
 
+#[derive(Debug, Deserialize)]
+struct Cargo {
+    package: CargoPackage,
+}
+
+impl Cargo {
+    fn load(fpath: &Path) -> Self {
+        let mut f = File::open(fpath).expect("file not found");
+        let mut contents = String::new();
+        f.read_to_string(&mut contents)
+            .expect("something went wrong reading the file");
+
+        /// toml validation
+        match contents.parse::<Toml>() {
+            Ok(toml_string) => {}
+            Err(error) => panic!("failed to parse TOML: {}", error),
+        }
+
+        let this: Self = toml::from_str(&contents).expect("failed to parse TOML");
+        this
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct CargoPackage {
+    version: String,
+}
+
 fn main() {
     match Opt::from_args() {
         Opt::Up {
@@ -57,8 +86,27 @@ fn main() {
             minor,
             patch,
         } => {
-            let ver_s = "1.2.3";
-            let mut ver = Version::parse(ver_s).unwrap();
+            let fpath = Path::new("Cargo.toml");
+            let mut f = File::open(fpath).expect("file not found");
+            let mut contents = String::new();
+            f.read_to_string(&mut contents)
+                .expect("something went wrong reading the file");
+
+            /// toml validation
+            match contents.parse::<Toml>() {
+                Ok(toml_string) => {}
+                Err(error) => panic!("failed to parse TOML: {}", error),
+            }
+
+            let mut this: Toml = toml::from_str(&contents).expect("failed to parse TOML");
+            let ver_s = this
+                .get("package")
+                .unwrap()
+                .get("version")
+                .unwrap()
+                .as_str()
+                .unwrap();
+            let mut ver = Version::parse(&ver_s).unwrap();
             if major {
                 ver = ver.up_major(1);
             }
@@ -68,7 +116,18 @@ fn main() {
             if patch {
                 ver = ver.up_patch(1);
             }
-            dbg!(ver);
+            let mut ver_t = this
+                .get_mut("package")
+                .unwrap()
+                .get_mut("version")
+                .unwrap()
+                .as_str()
+                .unwrap();
+            ver_t = &ver.to_string();
+
+            let mut file = File::create("new.toml").unwrap();
+            let content = toml::to_string(&this).expect("Could not write to file");
+            writeln!(&mut file, "{}", content).unwrap();
         }
         _ => {}
     }
